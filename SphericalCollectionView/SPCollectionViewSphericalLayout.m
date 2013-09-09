c7428ab5a845bb02798f5a70f28bc629e2cf8c1a
direchlet <direchlet@gmail.com>
2013-09-08 21:28:51 -0500

    aligh all the views on a circle

--- SphericalCollectionView/SPCollectionViewSphericalLayout.m
+++ SphericalCollectionView/SPCollectionViewSphericalLayout.m
@@ -16,14 +16,32 @@
 }
 
 
+- (CGPoint) centerForIndex: (NSUInteger) index withMaxIndex: (NSUInteger) max
+{
+    CGFloat radius = MIN(self.collectionView.bounds.size.width, self.collectionView.bounds.size.height)/2 - 20;
+    CGFloat angle = (index + 0.01f)/ max * M_PI *2;
+    CGFloat xOffSet = cosf(angle) *radius;
+    CGFloat yOffSet = sinf(angle) *radius;
+    CGPoint point = self.collectionView.center;
+    point.x +=  xOffSet;
+    point.y += yOffSet;
+    return point;
+}
+
+
 - (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
 {
     NSArray *attributes =  [super layoutAttributesForElementsInRect:rect];
+    NSUInteger count = [attributes count];
+    
     [attributes enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-        NSLog(@"loop");
         UICollectionViewLayoutAttributes *attr = (UICollectionViewLayoutAttributes*) obj;
+        NSLog(@"x: %f, y: %f", attr.center.x, attr.center.y);
+
         attr.alpha = (idx+0.01) / [attributes count];
+        attr.center = [self centerForIndex:idx withMaxIndex:count];
     }];
+    
     return attributes;
 }
 
